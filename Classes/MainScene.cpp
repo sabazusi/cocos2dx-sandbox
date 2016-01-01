051ca7be4ecb34499d285645ae1597293823c0ba
sabazusi <kouhei.live@gmail.com>
2016-01-01 20:06:56 +0900

    show body and arms.

--- Classes/MainScene.cpp
+++ Classes/MainScene.cpp
@@ -21,8 +21,8 @@ bool Main::init()
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
     
     // add "obj character" splash screen"
-    auto sprite = Sprite3D::create("chr_sword.obj");
-    sprite->setTexture("chr_sword.png");
+    auto sprite = Sprite3D::create("models/sushiman/sushiman-body.obj");
+    sprite->setTexture("models/sushiman/sushiman-body.png");
     auto rotation = RotateBy::create(10, Vec3(0, 360, 0));
     sprite->runAction(RepeatForever::create(rotation));
 
@@ -31,9 +31,21 @@ bool Main::init()
     
     // set scale
     sprite->setScale(3.0);
-
+    
     // add the sprite as a child to this layer
     this->addChild(sprite, 1);
+    
+    auto leftarm = Sprite3D::create("models/sushiman/sushiman-arm.obj");
+    leftarm->setTexture("models/sushiman/sushiman-arm.png");
+    auto spriteSize = sprite->getContentSize();
+    auto armSize = leftarm->getContentSize();
+    leftarm->setPosition(Vec2(armSize.width/2 + spriteSize.width/2 - 3, 0));
+    sprite->addChild(leftarm,1);
+    
+    auto rightarm = Sprite3D::create("models/sushiman/sushiman-arm.obj");
+    rightarm->setTexture("models/sushiman/sushiman-arm.png");
+    rightarm->setPosition(leftarm->getPosition() * -1);
+    sprite->addChild(rightarm);
     
     return true;
 };
