20f5407a67f3be68533c5eac50d42864fef3d2f7
sabazusi <kouhei.live@gmail.com>
2016-01-03 10:58:19 +0900

    revert character.

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -40,9 +40,9 @@ bool AppDelegate::applicationDidFinishLaunching() {
     auto glview = director->getOpenGLView();
     if(!glview) {
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32) || (CC_TARGET_PLATFORM == CC_PLATFORM_MAC) || (CC_TARGET_PLATFORM == CC_PLATFORM_LINUX)
-        glview = GLViewImpl::createWithRect("vatalog", Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
+        glview = GLViewImpl::createWithRect("sandbox", Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
 #else
-        glview = GLViewImpl::create("vatalog");
+        glview = GLViewImpl::create("sandbox");
 #endif
         director->setOpenGLView(glview);
     }
--- Classes/MainScene.cpp
+++ Classes/MainScene.cpp
@@ -21,8 +21,8 @@ bool Main::init()
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
     
     // add "obj character" splash screen"
-    auto sprite = Sprite3D::create("models/sushiman/sushiman-body.obj");
-    sprite->setTexture("models/sushiman/sushiman-body.png");
+    auto sprite = Sprite3D::create("models/chr_sword.obj");
+    sprite->setTexture("models/chr_sword.png");
     auto rotation = RotateBy::create(10, Vec3(0, 360, 0));
     sprite->runAction(RepeatForever::create(rotation));
 
@@ -34,18 +34,6 @@ bool Main::init()
     
     // add the sprite as a child to this layer
     this->addChild(sprite, 1);
-    
-    auto leftarm = Sprite3D::create("models/sushiman/sushiman-arm.obj");
-    leftarm->setTexture("models/sushiman/sushiman-arm.png");
-    auto spriteSize = sprite->getContentSize();
-    auto armSize = leftarm->getContentSize();
-    leftarm->setPosition(Vec2(armSize.width/2 + spriteSize.width/2 - 3, 0));
-    sprite->addChild(leftarm,1);
-    
-    auto rightarm = Sprite3D::create("models/sushiman/sushiman-arm.obj");
-    rightarm->setTexture("models/sushiman/sushiman-arm.png");
-    rightarm->setPosition(leftarm->getPosition() * -1);
-    sprite->addChild(rightarm);
     
     return true;
 };
